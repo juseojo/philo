3aa4c2b9526c7aca56a52a14607fd45adff598b9
juseojo <j86371224@gmail.com>
2022-08-19 17:41:50 +0900

    only_even and no_die

--- main.c
+++ main.c
@@ -6,33 +6,115 @@
 /*   By: seongjch <seongjch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 14:34:56 by seongjch          #+#    #+#             */
-/*   Updated: 2022/08/08 15:31:08 by seongjch         ###   ########.fr       */
+/*   Updated: 2022/08/19 17:39:46 by seongjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	philo_do(int philo_num)
+int check_can_eat(int max, int num, int *fork)
 {
-	printf("%d philo run\n", philo_num);
-	wait(100000);
+	if (num != max && fork[num] != 1 && fork[num + 1] != 1)
+	{
+		fork[num] = 1;
+		fork[num + 1] = 1;
+		return (1);
+	}
+	else if (num == max && fork[num] != 1 && fork[0] != 1)
+	{
+		fork[num] = 1;
+		fork[0] = 1;
+		return (1);
+	}
+	return (0);
+}
+
+void end_eat(int max, int num, int *fork)
+{
+	if (num != max)
+	{
+		fork[num] = 0;
+		fork[num + 1] = 0;
+	}
+	else if (num == max)
+	{
+		fork[num] = 0;
+		fork[0] = 0;
+	}
+}
+
+void	philo_do(t_vals vals)
+{
+	int	num;
+	int start;
+
+	start = vals.time;
+	num = vals.philo_num + 1;
+	//printf("%lld philo run\n", num);
+	while (1)
+	{
+		pthread_mutex_lock(&vals.mutex_lock);
+		if (check_can_eat(vals.args.number_of_philosophers - 1, num - 1, vals.fork))
+		{
+			pthread_mutex_unlock(&vals.mutex_lock);
+			printf("%lld %d has taken a fork\n", vals.time, num);
+			printf("%lld %d is eating\n", vals.time, num);
+			usleep(vals.args.time_to_eat * 1000);
+			pthread_mutex_lock(&vals.mutex_lock);
+			end_eat(vals.args.number_of_philosophers - 1, num - 1, vals.fork);
+			pthread_mutex_unlock(&vals.mutex_lock);
+			printf("%lld %d is sleeping\n", vals.time, num);
+			usleep(vals.args.time_to_sleep * 1000);
+			printf("%lld %d is thinking\n", vals.time, num);
+		}
+		pthread_mutex_unlock(&vals.mutex_lock);
+		usleep(100);
+	}
+}
+
+void time_do(t_vals vals)
+{
+	struct 			timeval start;
+
+	gettimeofday(&vals.tv, 0);
+	gettimeofday(&start, 0);
+	while (1)
+	{
+		gettimeofday(&vals.tv, 0);
+		pthread_mutex_lock(&vals.mutex_lock);
+		vals.time =	(vals.tv.tv_sec - start.tv_sec) * 1000 + ((vals.tv.tv_usec - start.tv_usec) / 1000);
+		pthread_mutex_unlock(&vals.mutex_lock);
+		usleep(100);
+	}
 }
 
 int	main(int argc, char *argv[])
 {
-	t_args			args;
-	pthread_mutex_t	mutex_lock;
-	pthread_t		*philos;
+	t_vals			vals;
+	pthread_t		*philo_threads;
+	pthread_t		time_thread;
 	int				cnt;
 
 	if (argc != 5)
-		exit(0);
-	args.number_of_philosophers = ft_atoi(argv[1]);
-	args.time_to_die = ft_atoi(argv[2]);
-	args.time_to_eat = ft_atoi(argv[3]);
-	args.time_to_sleep = ft_atoi(argv[4]);
-	philos = malloc(sizeof(pthread_t) * args.number_of_philosophers);
+		return(0);
+	vals.args.number_of_philosophers = ft_atoi(argv[1]);
+	vals.args.time_to_die = ft_atoi(argv[2]);
+	vals.args.time_to_eat = ft_atoi(argv[3]);
+	vals.args.time_to_sleep = ft_atoi(argv[4]);
+	pthread_mutex_init(&vals.mutex_lock, 0);
+	vals.fork = malloc(sizeof(int) * vals.args.number_of_philosophers);
+	vals.philos = malloc(sizeof(int) * vals.args.number_of_philosophers);
+	philo_threads = malloc(sizeof(pthread_t) * vals.args.number_of_philosophers);
+	if (!vals.fork || !vals.philos || !philo_threads)
+		exit (100);
 	cnt = -1;
-	while (++cnt < args.number_of_philosophers)
-		pthread_create((philos + cnt * 8), NULL, philo_do, cnt);
+	pthread_create(&time_thread, 0, time_do, &vals);
+	while (++cnt < vals.args.number_of_philosophers)
+	{
+		vals.philo_num = cnt;
+		pthread_create((philo_threads + cnt * sizeof(pthread_t)), 0, philo_do, &vals);
+		usleep(100);
+	}
+	usleep(100000000000000);
+	return (1);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: seongjch <seongjch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 14:48:43 by seongjch          #+#    #+#             */
-/*   Updated: 2022/08/08 15:04:27 by seongjch         ###   ########.fr       */
+/*   Updated: 2022/08/19 10:36:11 by seongjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <pthread.h>
+#include <sys/time.h>
+# include <unistd.h>
 
 typedef struct s_args {
 	long long	number_of_philosophers;
@@ -23,6 +25,16 @@ typedef struct s_args {
 	long long	time_to_eat;
 	long long	time_to_sleep;
 }	t_args;
+
+typedef struct s_vals {
+	t_args			args;
+	long long		philo_num;
+	long long		time;
+	int				*fork;
+	int				*philos;
+	struct 			timeval tv;
+	pthread_mutex_t mutex_lock;
+}	t_vals;
 
 int	ft_atoi(const char	*str);
 
